206495d518f1da80f023f3a918064aeb09654024
Matt Eland <matt.eland@gmail.com>
2022-07-09 16:56:27 -0400

    Probability test failing

--- MattEland.WhereDoggo/MattEland.WhereDoggo.Core.Tests/ProbabilityTests.cs
+++ MattEland.WhereDoggo/MattEland.WhereDoggo.Core.Tests/ProbabilityTests.cs
@@ -19,5 +19,25 @@ public class ProbabilityTests
         // Assert
         probabilities.Probabilities[RoleTypes.Werewolf].ShouldBe(2 / 6m);
         probabilities.Probabilities[RoleTypes.Villager].ShouldBe(4 / 6m);
+    }    
+    
+    [Test]
+    public void ProbabilityWithTwoRolesShouldDefaultToOtherRoleWhenOneRoleRuledOut()
+    {
+        // Arrange
+        ContainerRoleProbabilities probabilities = new(6);
+        Dictionary<RoleTypes, int> roleCounts = new()
+        {
+            [RoleTypes.Werewolf] = 2,
+            [RoleTypes.Villager] = 4
+        };
+
+        // Act
+        probabilities.MarkAsCannotBeRole(RoleTypes.Werewolf);
+        probabilities.RecalculateProbability(roleCounts);
+
+        // Assert
+        probabilities.Probabilities[RoleTypes.Werewolf].ShouldBe(0m);
+        probabilities.Probabilities[RoleTypes.Villager].ShouldBe(1m);
     }
 }
