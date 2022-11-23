85993fa5508a80ae839f3530e3dfc426eceaaa9a
Matt Eland <matt.eland@gmail.com>
2022-11-23 16:18:48 -0500

    Sizing Changes

--- MattEland.WhereDoggo/MattEland.WhereDoggo.WPFClient/MainWindow.xaml.cs
+++ MattEland.WhereDoggo/MattEland.WhereDoggo.WPFClient/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,14 @@ namespace MattEland.WhereDoggo.WPFClient
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ClickHyperlink(object sender, RequestNavigateEventArgs e)
+        {
+            // for .NET Core you need to add UseShellExecute = true
+            // see https://learn.microsoft.com/dotnet/api/system.diagnostics.processstartinfo.useshellexecute#property-value
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
